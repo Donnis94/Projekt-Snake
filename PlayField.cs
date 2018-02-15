ac5ced449dd2da2180c3b1d627f2e64526f51a23
tn222fe <tn222fe@student.lnu.se>
2018-02-15 15:53:58 +0100

    d

--- PlayField.cs
+++ PlayField.cs
@@ -14,7 +14,7 @@ namespace Snake_Projekt
         
         public PlayField(int AmountOfPlayers)
         {
-            
+            Players.Add(new Snake(1, 1, 1, null, System.Drawing.Color.AliceBlue));
         }
         
         public void Run()
