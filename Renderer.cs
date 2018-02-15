603305b159a20eab4b314df59ca767b90c6f7d29
tn222fe <tn222fe@student.lnu.se>
2018-02-15 14:19:53 +0100

    test

--- Renderer.cs
+++ Renderer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,14 @@ namespace Snake_Projekt
 {
     public class Renderer
     {
-        
+        public Renderer()
+        {
+
+        }
+
+        public void DrawAt(int x, int y, Pens p)
+        {
+
+        }
     }
 }
