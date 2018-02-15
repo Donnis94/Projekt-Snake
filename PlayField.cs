afef68b4ab09906255bb298259e4c314e1c17bbd
Dennis Lund <denniz_lund@hotmail.com>
2018-02-15 14:58:28 +0100

    Player

--- PlayField.cs
+++ PlayField.cs
@@ -8,5 +8,65 @@ namespace Snake_Projekt
 {
     class PlayField
     {
+        ISet<Snake> Players = new HashSet<Snake>();
+        ISet<Food> Food = new HashSet<Food>();
+        Timer timer;
+        Form1 playForm;
+
+        public PlayField(int AmountOfPlayers)
+        {
+            playForm = new Form1();
+            timer = new Timer();
+
+        }
+        
+        public void Run()
+        {
+            playForm.Paint += new PaintEventHandler;
+            
+        }
+
+        private bool IsGameOver()
+        {
+            foreach(var player in Players)
+            {
+                return player.IsAlive();
+            }
+        }
+
+        private void CheckCollisions()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Draw()
+        {
+            foreach(var player in Players)
+            {
+                player.Draw();
+            }
+            foreach(var food in Food)
+            {
+                food.Draw();
+            }
+        }
+
+        private void SpawnNewFood()
+        {
+
+        }
+
+        private void EndGame()
+        {
+            if(IsGameOver())
+            {
+                // Stop game somehow.
+            }
+        }
+
+        private TimeEventHandler(object sender, EventArgs args)
+        {
+
+        }
     }
 }
