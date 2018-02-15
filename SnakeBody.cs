e4736a68808291a98274c9e7873b874aa570d173
tn222fe <tn222fe@student.lnu.se>
2018-02-15 15:16:03 +0100

    SnakeBody

--- SnakeBody.cs
+++ SnakeBody.cs
@@ -10,7 +10,13 @@ namespace Snake_Projekt
     class SnakeBody : IDrawable
     {
         Pen pen = new Pen(Color.Black);
-        
+
+        Snake mySnake;
+
+        public SnakeBody(Snake _mySnake)
+        {
+            mySnake = _mySnake;
+        }
 
         public void Draw(Renderer renderer)
         {
