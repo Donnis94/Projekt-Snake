42897ea92f2b23be74714423e14b959366b62556
tn222fe <tn222fe@student.lnu.se>
2018-02-18 10:27:51 +0100

    Put all controllers in 1 file

--- Controller.cs
+++ Controller.cs
@@ -9,10 +9,76 @@ namespace Snake_Projekt
 {
     abstract class Controller
     {
-       
+
         public Snake.Direction direction;
 
         public abstract void ValidMove(Keys key);
 
+    }
+
+    class Controller1 : Controller
+    {
+        public override void ValidMove(Keys key)
+        {
+            switch (key)
+            {
+                case Keys.Left:
+                    direction = Snake.Direction.left;
+                    break;
+                case Keys.Right:
+                    direction = Snake.Direction.right;
+                    break;
+                case Keys.Up:
+                    direction = Snake.Direction.up;
+                    break;
+                case Keys.Down:
+                    direction = Snake.Direction.down;
+                    break;
+            }
+        }
+    }
+
+    class Controller2 : Controller
+    {
+        public override void ValidMove(Keys key)
+        {
+            switch (key)
+            {
+                case Keys.A:
+                    direction = Snake.Direction.left;
+                    break;
+                case Keys.D:
+                    direction = Snake.Direction.right;
+                    break;
+                case Keys.W:
+                    direction = Snake.Direction.up;
+                    break;
+                case Keys.S:
+                    direction = Snake.Direction.down;
+                    break;
+            }
+        }
+    }
+
+    class Controller3 : Controller
+    {
+        public override void ValidMove(Keys key)
+        {
+            switch (key)
+            {
+                case Keys.J:
+                    direction = Snake.Direction.left;
+                    break;
+                case Keys.L:
+                    direction = Snake.Direction.right;
+                    break;
+                case Keys.I:
+                    direction = Snake.Direction.up;
+                    break;
+                case Keys.K:
+                    direction = Snake.Direction.down;
+                    break;
+            }
+        }
     }
 }
