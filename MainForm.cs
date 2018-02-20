c586af2323043a68e0a7852dd3abef37752e92dc
Donatello Piancazzo <dpo16001@student.mdh.se>
2018-02-20 11:21:47 +0100

    Form Design

--- MainForm.cs
+++ MainForm.cs
@@ -16,7 +16,7 @@ namespace Snake_Projekt
         Timer timer;
         Renderer r;
 
-        FlowLayoutPanel flowPanel;
+        
 
         private int TilesX = 50;
         private int TilesY = 50;
@@ -28,7 +28,6 @@ namespace Snake_Projekt
             Height = 2000;
             BackColor = Color.Black;
             InitializeComponent();
-            InitializeGUI();
             Initialize();
             KeyDown += pf.MainForm_KeyPress;
             Resize += MainForm_Resize;
@@ -36,10 +35,10 @@ namespace Snake_Projekt
 
         private void Initialize()
         {
-            flowPanel.Paint += new PaintEventHandler(Draw);
+            MainForm_Label.Paint += new PaintEventHandler(Draw);
 
             pf = new PlayField(1, TilesX, TilesY);
-
+            r = new Renderer(MainForm_Label, TilesX, TilesY);
             timer = new Timer();
             timer.Tick += new EventHandler(TimerEventHandler);
             timer.Interval = 1000 / 20;
@@ -47,26 +46,7 @@ namespace Snake_Projekt
 
         }
 
-        private void InitializeGUI()
-        {
-
-            FlowLayoutPanel mainPanel = new FlowLayoutPanel();
-            mainPanel.Height = 100;
-            flowPanel = new FlowLayoutPanel();
-
-            mainPanel.BackColor = Color.ForestGreen;
-            
-            mainPanel.Dock = DockStyle.Top;
-            Label score = new Label();
-            score.Container.ToString();
-            mainPanel.Controls.Add(score);
-            flowPanel.Dock = DockStyle.Fill;
-            flowPanel.BackColor = Color.White;
-
-            this.Controls.Add(flowPanel);
-            this.Controls.Add(mainPanel);
-            r = new Renderer(flowPanel, TilesX, TilesY);
-        }
+        
 
         private void Draw(Object obj, PaintEventArgs args)
         {
@@ -76,7 +56,7 @@ namespace Snake_Projekt
         private void TimerEventHandler(Object obj, EventArgs args)
         {
             pf.GameLoop();
-            flowPanel.Refresh();
+            MainForm_Label.Refresh();
         }
 
         private void MainForm_Resize(object sender, EventArgs e)
